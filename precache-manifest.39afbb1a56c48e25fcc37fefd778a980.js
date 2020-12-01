2052dc36fd9d7ebff0b128f8f59493153ee48f78
pantojao <61061684+pantojao@users.noreply.github.com>
2020-12-01 14:40:42 -0800

    Updates

---
+++ precache-manifest.39afbb1a56c48e25fcc37fefd778a980.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15fb3b574f11a4be8f5d7f28c3d02bb",
+    "url": "/ipa-app/index.html"
+  },
+  {
+    "revision": "447efc494350f8dc9065",
+    "url": "/ipa-app/static/css/main.5982c4f3.chunk.css"
+  },
+  {
+    "revision": "3e723285db84d396b55d",
+    "url": "/ipa-app/static/js/2.a00c6c4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ipa-app/static/js/2.a00c6c4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "447efc494350f8dc9065",
+    "url": "/ipa-app/static/js/main.b38592bd.chunk.js"
+  },
+  {
+    "revision": "f00928e941e6f5b89949",
+    "url": "/ipa-app/static/js/runtime-main.ef63420b.js"
+  },
+  {
+    "revision": "7729ac1a28afbf73f76c3031c3e73fbc",
+    "url": "/ipa-app/static/media/ipa-header.7729ac1a.png"
+  }
+]);
